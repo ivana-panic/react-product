364dfed925c0172c8b131a31d085defd2cdf37f6
Ivana Panic <ivanapanic97@hotmail.com>
2021-11-01 00:44:24 +0100

    create list products

--- components/products.js
+++ components/products.js
@@ -1,3 +1,38 @@
 import { Product } from "./product";
 
-export const Products = () => {};
+export const Products = () => {
+  const listProduct = [
+    {
+      name: "Doctor Sleep",
+      author: "Stephen King",
+      image: "image/Doctor_Sleep.jpeg",
+      description:
+        "Doctor Sleep is a 2013 horror novel by American writer Stephen King and the sequel to his 1977 novel The Shining. The book reached the first position on The New York Times Best Seller list for print and ebook fiction (combined), hardcover fiction, and ebook fiction",
+      price: "10e",
+    },
+    {
+      name: "Cockroaches",
+      author: "Jo Nesbo",
+      image: "image/The_Cockroaches.jpeg",
+      description:
+        "Cockroaches (Norwegian: Kakerlakkene, 1998) is a crime novel by Norwegian writer Jo Nesbø, the second in the Harry Hole series.",
+      price: "8e",
+    },
+    {
+      name: "The Girl on the Train",
+      author: "Paula Hawkins",
+      image: "image/The_Girl_On_The_Train.png",
+      description:
+        "The Girl on the Train is a 2015 psychological thriller novel by British author Paula Hawkins that gives narratives from three different women about relationship troubles and binge drinking",
+      price: "11e",
+    },
+    {
+      name: "Tell Me Your Dreams",
+      author: "Sidney Sheldon",
+      image: "image/Tell_Me_Your_Dreams.jpeg",
+      description:
+        "The main characters of the book are Ashley Patterson, an introverted workaholic, her co-workers, Toni Prescott, an outgoing singer and dancer, shy artist Alette Peters and Ashley's father, Dr. Steven Patterson.",
+      price: "6e",
+    },
+  ];
+};
